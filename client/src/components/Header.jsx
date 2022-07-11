4e871c99809de2ac8ce85ff345837ddd41f54ac0
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-07-11 18:30:51 -0400

    UpdateHeader component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Header = () => {
   return (
-    <div className={"row"}>
-      <p className={'page-header'}>Trello Dashboard</p>
+    <div className={'row'}>
+      <p className={'page-header'}>Trello Dashboard 🗂</p>
     </div>
-  )
-}
+  );
+};
+
+export default Header;
