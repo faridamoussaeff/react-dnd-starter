8b13dc5ac5a5263f7266d9227c44db86e8a4ada4
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-07-11 18:34:13 -0400

    Imported dependencies and rendered them inside app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import Homepage from "./pages/Homepage";
+import React from 'react';
+import Homepage from './pages/Homepage';
+import Header from './components/Header';
+import { DndProvider } from 'react-dnd';
+import Backed from 'react-dnd-html5-backend';
 
 const App = () => {
-    return (
-        <Homepage />
-    );
+  return (
+    <DndProvider backend={Backed}>
+      <Header />
+      <Homepage />
+    </DndProvider>
+  );
 };
 
 export default App;
